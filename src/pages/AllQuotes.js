9042a81022d083da50b251cdffb64b41576cf505
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-03-12 23:05:46 +0300

    feat: added transition to the details of the quote

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 const AllQuotes = () => {
   return (
     <section>
       <h1>All Quotes</h1>
+      <ul>
+        <li>
+          <Link to="/quotes/q1">Quote 1</Link>
+        </li>
+        <li>
+          <Link to="/quotes/q2">Quote 2</Link>
+        </li>
+        <li>
+          <Link to="/quotes/q3">Quote 3</Link>
+        </li>
+      </ul>
     </section>
   );
 };
--- src/pages/DetailQuote.js
+++ src/pages/DetailQuote.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const DetailQuote = () => {
+  const param = useParams();
+
   return (
     <section>
       <h1>Detail Quote</h1>
+      <p>{param.quoteID}</p>
     </section>
   );
 };
