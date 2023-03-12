2faa0dbcd4c33033fa594224ca08923871a6e0cd
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-03-13 00:05:43 +0300

    feat: add layout wrapper component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { Redirect, Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import DetailQuote from "./pages/DetailQuote";
 import NewQuote from "./pages/NewQuote";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes"/>
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteID">
-        <DetailQuote />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteID">
+          <DetailQuote />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
