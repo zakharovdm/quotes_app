64c2d40456e6cc46a46cecc1598de03478deb03c
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-03-13 23:32:24 +0300

    feat: add imperative navigation after added new quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,14 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
   const onAddHandler = (dataQuote) => {
     console.log(dataQuote);
-  }
+
+    history.push("/quotes");
+  };
 
   return <QuoteForm onAddQuote={onAddHandler} />;
 };
