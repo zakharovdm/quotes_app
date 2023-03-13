32e1ba543690f406abcd2067c7c1142996f6b183
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-03-13 23:01:50 +0300

    feat: add detail page component

--- src/pages/DetailQuote.js
+++ src/pages/DetailQuote.js
@@ -1,13 +1,24 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DetailQuote = () => {
   const param = useParams();
 
+  const DUMMY_QUOTES = [
+    { id: "q1", author: "Max", text: "Learning React is fun" },
+    { id: "q2", author: "Jhon", text: "Learning React is great" },
+  ];
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === param.quoteID);
+
+  if (!quote) {
+    return <p>Not found quote</p>
+  }
+
   return (
     <section>
-      <h1>Detail Quote</h1>
-      <p>{param.quoteID}</p>
+      <HighlightedQuote author={quote.author} text={quote.text}/>
       <Route path={`/quotes/${param.quoteID}/comments`}>
         <Comments />
       </Route>
