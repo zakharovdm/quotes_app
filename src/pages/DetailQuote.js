de515dcfe2be60bf93ecad0394699cce7996f45c
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-03-14 22:33:06 +0300

    feat: add comments component

--- src/pages/DetailQuote.js
+++ src/pages/DetailQuote.js
@@ -1,25 +1,32 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DetailQuote = () => {
-  const param = useParams();
+  const params = useParams();
 
   const DUMMY_QUOTES = [
     { id: "q1", author: "Max", text: "Learning React is fun" },
     { id: "q2", author: "Jhon", text: "Learning React is great" },
   ];
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === param.quoteID);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteID);
 
   if (!quote) {
-    return <p>Not found quote</p>
+    return <p>Not found quote</p>;
   }
 
   return (
     <section>
-      <HighlightedQuote author={quote.author} text={quote.text}/>
-      <Route path={`/quotes/${param.quoteID}/comments`}>
+      <HighlightedQuote author={quote.author} text={quote.text} />
+      <Route path={`/quotes/${params.quoteID}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteID}/comments`}>
+            Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`/quotes/${params.quoteID}/comments`}>
         <Comments />
       </Route>
     </section>
